eab5d30e53e5bf6facdaf0ce02f946a3c9c23301
-VPN <132519868+hazhan4268@users.noreply.github.com>
2026-06-26 04:47:07 +0330

    Add one-line installer bootstrap

---
+++ install-online.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [[ $EUID -ne 0 ]]; then
+  exec sudo -E bash "$0" "$@"
+fi
+
+REPO_URL="${TUNNELMOD_REPO_URL:-https://github.com/hazhan4268/tunnelmod.git}"
+SOURCE_DIR="${TUNNELMOD_SOURCE_DIR:-/opt/tunnelmod-src}"
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y git ca-certificates curl
+
+if [[ -d "$SOURCE_DIR/.git" ]]; then
+  git -C "$SOURCE_DIR" fetch --prune origin main
+  git -C "$SOURCE_DIR" checkout main
+  git -C "$SOURCE_DIR" merge --ff-only origin/main
+else
+  rm -rf "$SOURCE_DIR"
+  git clone "$REPO_URL" "$SOURCE_DIR"
+fi
+
+cd "$SOURCE_DIR"
+exec bash ./install.sh "$@"
